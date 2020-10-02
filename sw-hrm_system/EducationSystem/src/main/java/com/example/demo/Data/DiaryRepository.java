adfbe8a3d3c5c0c3f590de234ad8e4c3f7460f5f
kuwabara <kuwabara799@gmail.com>
2020-10-02 15:09:26 +0900

    最新版

--- sw-hrm_system/EducationSystem/src/main/java/com/example/demo/Data/DiaryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiaryRepository extends JpaRepository<Diary, Integer> {
-
-}
--- sw-hrm_system/EducationSystem/src/main/resources/front/src/js/index.js
+++
@@ -1,4 +0,0 @@
-import "../scss/style.scss";
-//import jQuery from 'jquery';
-import "./topPage.js";
-import "./indicatePassword.js";
